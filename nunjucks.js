53f50cbeb70ae8dfac69c672242b1e1fe2924734
John O'Connor <sax1johno@gmail.com>
2016-07-28 01:56:52 -0700

    Added nunjucks templates since jade was deprecated and pug isnt ready yet

---
+++ nunjucks.js
@@ -0,0 +1,54 @@
+/**
+ * A render engine for the 'views' engine that allows for rendering with the 
+ * jade template engine.  It uses a custom parser that modifies the 'include'
+ * and 'extends' methods to use other stored views rather than the file system.
+ **/
+
+var underscore = require('underscore'),
+    path = require('path'),
+    fs = require('fs'),
+    async = require('async'),
+    temp = require('temp'),
+    sutil  = require('util'),
+    nunjucks = require('nunjucks'),
+    sync = require('synchronize'),
+    markdown = require('marked');
+    
+module.exports = function(options) {
+    var pluginName = "views";
+    
+    var seneca = this;
+    
+    options = seneca.util.deepextend({
+    },options)
+
+    /**
+     * Render the specified view with the specified render engine.
+     **/
+    seneca.add({
+        role: pluginName, 
+        cmd: 'renderWithEngine', 
+        engine: 'njk',
+        view: {required$: true}
+    }, function(args, done) {
+        seneca.log.debug("Rendering with njk");
+        var locals = args.locals || {};
+        // Add markdown support.  $ at the end is meant to prevent accidental namespace overloading by other
+        // locals.
+        locals.markdown$ = markdown;
+        locals.md$ = markdown;
+        sync.fiber(function() {
+            try {
+                options.seneca = seneca;
+                nunjucks.renderString(args.view.template, locals, function(err, html) {
+                    console.log("Html = ", sutil.inspect(html));
+                    done(err, {html: html});
+                });
+            } catch (e) {
+                done(null, {error: "Render Error", html: "<h1>Error Encountered</h1><p>Unable to render view: </p><p>" + e + "</p>"});
+            }
+        });
+    });
+    
+    return pluginName;
+}
---
+++ pug.js
@@ -0,0 +1,75 @@
+/**
+ * A render engine for the 'views' engine that allows for rendering with the 
+ * pug template engine.  It uses a custom parser that modifies the 'include'
+ * and 'extends' methods to use other stored views rather than the file system.
+ **/
+
+var underscore = require('underscore'),
+    path = require('path'),
+    fs = require('fs'),
+    async = require('async'),
+    temp = require('temp'),
+    sutil  = require('util'),
+    pug = require('pug'),
+    sync = require('synchronize'),
+    pugParserPatch = require('./lib/pugAdapter'),
+    markdown = require('marked');
+    
+module.exports = function(options) {
+    var pluginName = "views";
+    
+    var seneca = this;
+    
+    options = seneca.util.deepextend({
+    },options)
+
+    var pugRender = function(args, done) {
+        seneca.log.debug("Rendering with pug");
+        var locals = args.locals || {};
+        // Add markdown support.  $ at the end is meant to prevent accidental namespace overloading by other
+        // locals.
+        locals.markdown$ = markdown;
+        locals.md$ = markdown;
+        sync.fiber(function() {
+            try {
+                options.parser = pugParserPatch;
+                options.seneca = seneca;
+                var fn = pug.compile(args.view.template, options);
+                console.log("fn = ", fn);
+                console.log("locals = ", locals);
+                var html = fn(locals);
+                console.log("Html = ", sutil.inspect(html));
+                done(null, {html: html});
+            } catch (e) {
+                done(null, {error: "Render Error", html: "<h1>Error Encountered</h1><p>Unable to render view: </p><p>" + e + "</p>"});
+            }
+        });
+    }
+
+    /**
+     * Render the specified view with the specified render engine.
+     **/
+    seneca.add({
+        role: pluginName, 
+        cmd: 'renderWithEngine', 
+        engine: 'jade',
+        view: {required$: true}
+    }, 
+    pugRender 
+    });
+
+    /**
+     * Render the specified view with the specified render engine.
+     **/
+    seneca.add({
+        role: pluginName, 
+        cmd: 'renderWithEngine', 
+        engine: 'pug',
+        view: {required$: true}
+    }, 
+    pugRender 
+    })
+
+    
+    return pluginName;
+}
