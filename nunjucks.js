157b6b3dcdd7f104086168fd4463cf6dc1323d25
John O'Connor <sax1johno@gmail.com>
2016-10-18 10:13:43 -0700

    Hopefully fixed a bug that caused variable rendering in subtemplates to fail.

--- nunjucks.js
+++ nunjucks.js
@@ -61,7 +61,7 @@ module.exports = function(options) {
                     }
                     // var renderedTemplate = sync.await(nunjucks.renderString(list[0], {}, sync.defer()));
                     // return renderedTemplate;
-                    var tmpl = new renderEnv.Template(list[0].template);
+                    var tmpl = new nunjucks.Template(list[0].template, renderEnv);
                     return tmpl;
                 }
                 renderEnv.renderString(args.view.template, locals, function(err, html) {
